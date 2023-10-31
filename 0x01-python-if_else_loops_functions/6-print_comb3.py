dc3ea7f718bfbc92232d258f3a14c0b1e5f0112d
mouadeso <mouadesoubai123gmail.com>
2023-10-31 16:22:21 -0700

    test

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,5 @@
 
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, ones_digit), end=", " if tens_digit < 8 or ones_digit < 9 else "\n")
+        separator = ", " if tens_digit < 8 or ones_digit < 9 else "\n"
+        print("{:d}{:d}".format(tens_digit, ones_digit), end=separator)
