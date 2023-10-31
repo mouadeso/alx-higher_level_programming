9e0cb14f254412c9fd4cc44fadd4de067acdefe4
mouadeso <mouadesoubai123gmail.com>
2023-10-31 15:31:40 -0700

    test

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range( 100):
-    if i < 99:
-     print("{:02},".format(i),end=" ")
+for number in range(100):
+    if number < 99:
+        print(f"{number:02}, ", end="")
     else:
-     print("{:02}".format(i),end=" ")
+        print(f"{number:02}")
