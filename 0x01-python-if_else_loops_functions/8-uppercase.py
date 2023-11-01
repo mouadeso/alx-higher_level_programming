ac7ef3f4962ff638170f09e9b0d08daa5276f4f1
mouadeso <mouadesoubai123gmail.com>
2023-11-01 04:08:42 -0700

    test

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for char in str:
+        if 'a' <= char <= 'z':
+            # Convert lowercase letters to uppercase by adjusting ASCII code
+            upper_char = chr(ord(char) - ord('a') + ord('A'))
+        else:
+            upper_char = char
+        print(upper_char, end="")
+    print()
