9e43c461e06702f9413d6f279a896205b094a66a
mouadeso <mouadesoubai123gmail.com>
2023-11-01 10:20:11 -0700

    test

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
+output = ""
 for i in range(ord('z'), ord('A') - 1, -1):
+    output += "{}{}".format(chr(i) if (i - ord('z')) % 2 == 0 else chr(i).upper())
+print(output, end='')
