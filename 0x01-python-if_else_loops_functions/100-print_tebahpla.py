27f177c9448bc017c4ad2221fdc44987281aba3c
mouadeso <mouadesoubai123gmail.com>
2023-11-01 10:22:42 -0700

    test

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-output = ""
-for i in range(ord('z'), ord('A') - 1, -1):
-    output += "{}{}".format(chr(i) if (i - ord('z')) % 2 == 0 else chr(i).upper())
-print(output, end='')
+# 100-print_tebahpla.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+""""Print the alphabet in reverse order alternating upper- and lower-case."""
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
