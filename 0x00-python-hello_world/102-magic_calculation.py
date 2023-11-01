63abcc0c27b28a2bfec95d57098b8899958e7edc
mouadeso <mouadesoubai123gmail.com>
2023-11-01 10:34:41 -0700

    test

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,13 +1,2 @@
-#!/usr/bin/python3
-import dis
-
 def magic_calculation(a, b):
-    result = 98  # LOAD_CONST 1 (98)
-    result += a  # LOAD_FAST 0 (a)
-    result = result ** b  # BINARY_POWER
-    result += a  # BINARY_ADD
-    return result  # RETURN_VALUE
-
-# Check if it produces the same bytecode
-dis.dis(magic_calculation)
-
+    return (98 + a ** b)
