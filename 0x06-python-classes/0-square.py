818475b66842940d291b832b62257ced15c18bbd
mouadeso <mouadesoubai123gmail.com>
2023-11-22 04:38:03 -0800

    test

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-This module defines a Square class.
-"""
-
 class Square:
-    """
-    This class represents a square.
-    """
+    """Empty class that defines a square"""
     pass
